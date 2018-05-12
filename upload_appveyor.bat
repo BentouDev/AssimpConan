e951937ed15463e9f4365ae55bf317573e3633a2
bentoo <bentkowski.jakub@gmail.com>
2018-05-13 00:23:28 +0200

    Signed repository key

--- upload_appveyor.bat
+++ upload_appveyor.bat
@@ -1,4 +1,3 @@
-#!/bin/bash
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
-
+conan user -p %REPOSITORY_KEY% -r yage bentoudev
 conan upload Assimp/0.1@bentoudev/yage --all -r=yage
