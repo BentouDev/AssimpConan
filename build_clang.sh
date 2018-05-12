02a8ed28b681b2d8fd89c7e912b68b4591c32e42
bentoo <bentkowski.jakub@gmail.com>
2018-05-13 01:05:02 +0200

    pass libcxx

--- build_clang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CXX=clang++-5.0
-export CC=clang-5.0
-$CXX --version
-cmake --version
-conan --version
-conan create . bentoudev/yage -s compiler=clang
--- build_gcc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CXX=g++
-export CC=gcc
-$CXX --version
-cmake --version
-conan --version
-conan create . bentoudev/yage -s compiler=gcc
