5188f1e7cd056540d037f3efd985fd0b78881860
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-05-13 03:14:42 +0200

    indent fix

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,8 @@ class AssimpConan(ConanFile):
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
         tools.replace_in_file("%s/CMakeLists.txt" % ("assimp"), "PROJECT( Assimp )", """PROJECT( Assimp )
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()""")
+        include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+        conan_basic_setup()""")
 
     def build(self):
         cmake = CMake(self)
