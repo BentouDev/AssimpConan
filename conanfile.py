f17b154e300731924b7ca9fdce2f16f836ff44fe
BentouDev <bentkowski.jakub@gmail.com>
2019-04-21 12:41:15 +0200

    First draft of moving to new CI scripts

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,24 @@
 from conans import ConanFile, CMake, tools
+import os, platform
+
+assimp_version = os.getenv('ASSIMP_VERSION', '0.0')
+assimp_commit = os.getenv('ASSIMP_COMMIT', '')
 
 class AssimpConan(ConanFile):
-    name = "Assimp"
-    version = "0.1"
+    name = "assimp"
     license = "MIT"
-    homepage = "https://github.com/assimp/assimp"
     url = "https://github.com/BentouDev/AssimpConan"
+    version = assimp_version
+    commit = assimp_commit
+
     description = "Assimp conan package"
+    homepage = "https://github.com/assimp/assimp"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
     generators = "cmake"
+    exports_sources = ["assimp-source/*"]
+
+    options = {"shared": [True, False],"build_type": ["Release", "Debug", "RelWithDebInfo", "MinSizeRel"]}
+    default_options = {"build_type":"MinSizeRel", "shared":"True"}
 
     def build_id(self):
         # Produce different package id for each configuration
@@ -19,17 +27,22 @@ class AssimpConan(ConanFile):
         self.info_build.settings.arch = "Any"
         self.info_build.settings.os = "Any"
 
-    def source(self):
-        self.run("git clone https://github.com/assimp/assimp.git")
+    def fix_linkage(self):
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
-        tools.replace_in_file("%s/CMakeLists.txt" % ("assimp"), "PROJECT( Assimp )", 
+        tools.replace_in_file("%s/CMakeLists.txt" % ("assimp-source"), "PROJECT( Assimp )", 
 
 """PROJECT( Assimp )
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()""")
 
     def build(self):
+        # Workaround for conan choosing cmake embedded in Visual Studio
+        if platform.system() == "Windows" and 'AZURE' in os.environ:
+            cmake_path = '"C:\\Program Files\\CMake\\bin\\cmake.exe"'
+            print (' [DEBUG] Forcing CMake : ' + cmake_path)
+            os.environ['CONAN_CMAKE_PROGRAM'] = cmake_path
+
         cmake = CMake(self)
         cmake.definitions["ASSIMP_BUILD_TESTS"] = "OFF"
         cmake.definitions["ASSIMP_BUILD_SAMPLES"] = "OFF"
@@ -40,14 +53,14 @@ conan_basic_setup()""")
             #elif self.settings.compiler == "Visual Studio":
             #    cmake.definitions["CONAN_CXX_FLAGS"].append("/bigobj")
 
-        cmake.configure(source_folder = "assimp")
+        cmake.configure(source_folder = "assimp-source")
         cmake.build()
 
     def package(self):
         # source code
-        self.copy("*.h", dst="include", src="assimp/include")
-        self.copy("*.hpp", dst="include", src="assimp/include")
-        self.copy("*.inl", dst="include", src="assimp/include")
+        self.copy("*.h", dst="include", src="assimp-source/include")
+        self.copy("*.hpp", dst="include", src="assimp-source/include")
+        self.copy("*.inl", dst="include", src="assimp-source/include")
 
         # generated config.h file
         self.copy("*.h", dst="include", src="include")
--- test_package/example.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <assimp/DefaultLogger.hpp>
-
-int main()
-{
-    auto* logger = Assimp::DefaultLogger::create();
-    std::cout << (void*)logger << std::endl;
-    return 0;
-}
